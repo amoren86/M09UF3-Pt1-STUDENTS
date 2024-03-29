8f941ea073adda0e8ac5e6b6763b9dabcffd12c9
Toni <amoren86@xtec.cat>
2024-03-29 19:13:40 +0100

    2024.03.29

---
+++ downloader/src/cat/institutmarianao/downloader/Downloader.java
@@ -0,0 +1,22 @@
+package cat.institutmarianao.downloader;
+
+import java.io.IOException;
+
+public class Downloader {
+
+	public void addResourceType(String string, String string2) {
+		// TODO
+
+	}
+
+	public void createFolderTree() throws IOException {
+		// TODO
+		throw new IOException();
+	}
+
+	public Thread saveResource(String resource) throws IOException {
+		// TODO
+		throw new IOException();
+	}
+
+}
